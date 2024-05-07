834f774173d0dfdf1ced943aad884ab48a6e0f7f
minhtriet23120380 <166209900+MinhTriet24@users.noreply.github.com>
2024-05-07 20:53:02 +0700

    Update Console.h

--- BonusProject/main/Console.h
+++ BonusProject/main/Console.h
@@ -1,10 +1,12 @@
 #ifndef Console_h
 #define Console_h
 //.Console la luu nhung ham thao tac hien thi tren man hinh console va cac ham xu li ngay(Date) thi luu luon trong nay
+
 void hideCursor(bool isHiden);
 void getConsoleSize(int& width, int& height);
 void gotoXY(int x, int y);
 void drawBox(int width, int height, int left, int top);
 void textAlignCenter(string s, int left, int width, int y);
+string DateToString(Date d);
 
 #endif
