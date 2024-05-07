e5e36bd085c792eb0faf6580e98164d790515bbd
minhtriet23120380 <166209900+MinhTriet24@users.noreply.github.com>
2024-05-07 20:34:50 +0700

    Update Login.cpp

--- BonusProject/main/Login.cpp
+++ BonusProject/main/Login.cpp
@@ -26,3 +26,20 @@ User* login(string id, string password) {
 	}
 	return NULL;
 }
+
+User* login(string id, string password) //kiem tra xem co ton tai id va mat khau trong danh sach nguoi dung hay khong
+{
+	User* account = listUser.pHead; //khoi tao bien chay la node dau cua danh sach nguoi dung
+	while(account!=NULL) //lap den het danh sach
+		{
+			if(id==account->id) //truong hop gap id cua nguoi dung trong danh sach trung voi id duoc nhap
+			{
+				if(password == account->password) return account;//neu trung mat khau thi tra va nguoi dung do
+				else return NULL;// khong trung mat khau nghia la nguoi dung nhap sai vi id chi co 1, tra ve node null
+			}
+			account = account->pNext;//chuyen qua nguoi dung ke tiep trong danh sach
+		}
+	return NULL; //sau khi chay vong lap ma khong tim thay user trung voi tai khoan duoc nhap thi tra ve null
+}
+
+
