ee8571bf256f62ec12a9f53efda29c582718f083
thachy296 <162892794+ThachY-23120411@users.noreply.github.com>
2024-05-07 13:29:57 +0700

    Delete BonusProject directory

--- BonusProject/BonusProject/Login.h
+++
@@ -1,7 +0,0 @@
-#include "Struct.h"
-#include"Console.h"
-void loginSystem();
-void loginUI();
-User* login(string id, string password);
-string getPassword(bool isHidden);
-
--- BonusProject/BonusProject/User.h
+++
@@ -1,6 +0,0 @@
-#include "Struct.h"
-
-void initList(ListUser& list);
-User* convertUserData(ifstream& data);
-void addUser(ListUser& list, User* user);
-void getListUsers();
