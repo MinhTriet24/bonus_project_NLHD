7bea3bc8d6830f01319019fe4e1fa8c4de30d347
thachy296 <162892794+ThachY-23120411@users.noreply.github.com>
2024-05-10 16:59:55 +0700

    Update Login.cpp

--- BonusProject/main/Login.cpp
+++ BonusProject/main/Login.cpp
@@ -34,7 +34,7 @@ string getPassword(bool isHidden) {
 		else cout << " ";
 		// Đưa con trỏ về sau PASSWORD: 
 		gotoXY(58 + passwrd.length(), yPos + 2);
-		char ch = getch();
+		char ch = _getch();
 		// Tạm dừng để nhập ký tự
 		if (ch == 13) {
 			// Ký tự enter. Nhập xong password. Trả về password luôn
@@ -120,67 +120,67 @@ void loginUI() {
 	password = getPassword(true);
 	currentUser = login(id, password);
 }
-void SiteToLogin() {
-    loginUI();
-    vector<string> options = { "User account","Profile","Manage Student","Manage Course","Setting","Exit" };
-    int currentOption = 0;
-    int onMenu3 = 0;
-    char key;
-    do {
-        if (onMenu3 == 0) {
-            printMenu(options, currentOption,10);
-            drawBox(40, 10, 30, 7);
-            key = _getch(); // Nhận phím từ người dùng mà không cần nhấn Enter
+void SiteOfStudent() {
+	vector<string> options = { "User account","Profile","Manage Student","Manage Course","Setting","Exit" };
+	int currentOption = 0;
+	int onMenu3 = 0;
+	char key;
+	do {
+		int k = 9;
+		if (onMenu3 == 0) {
+			printMenu(options, currentOption, k);
+			drawBox(40, 10, 40, 7);
+			key = _getch(); // Nhận phím từ người dùng mà không cần nhấn Enter
 
-            switch (key) {
-            case 'w':
-                if (currentOption > 0) {
-                    currentOption--;
-                }
-                break;
-            case 's':
-                if (currentOption < options.size() - 1) {
-                    currentOption++;
-                }
-                break;
-            case 13: // Enter
-                if (currentOption == 1) { // Chọn Option 2
-                    onMenu3 = 2; // Đặt biến onMenu thành false để chuyển đến trang mới
-                }
-                if (currentOption == 0) {//.Option 1
-                    onMenu3 = 1;
-                }
-                if (currentOption == 2) {//.OP 3
-                    onMenu3 = 3;
-                }
-                if (currentOption == 3) {
-                    onMenu3 = 4;
-                }
-                if (currentOption == 4) {
-                    onMenu3 = 5;
-                }
-                if (currentOption == 5) {//.current Option ma bang options.size()-1 thi la phan tu cuoi va chung la "Thoat".
-                    onMenu3 = 100;
-                    key = 'q';
-                }
-                break;
-            default:
-                break;
-            }
-        }
-        else if (onMenu3 == 2) {//.Option2
+			switch (key) {
+			case 'w':
+				if (currentOption > 0) {
+					currentOption--;
+				}
+				break;
+			case 's':
+				if (currentOption < options.size() - 1) {
+					currentOption++;
+				}
+				break;
+			case 13: // Enter
+				if (currentOption == 1) { // Chọn Option 2
+					onMenu3 = 2; // Đặt biến onMenu thành false để chuyển đến trang mới
+				}
+				if (currentOption == 0) {//.Option 1
+					onMenu3 = 1;
+				}
+				if (currentOption == 2) {//.OP 3
+					onMenu3 = 3;
+				}
+				if (currentOption == 3) {
+					onMenu3 = 4;
+				}
+				if (currentOption == 4) {
+					onMenu3 = 5;
+				}
+				if (currentOption == 5) {//.current Option ma bang options.size()-1 thi la phan tu cuoi va chung la "Thoat".
+					onMenu3 = 100;
+					key = 'q';
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		else if (onMenu3 == 2) {//.Option2
 
-        }
-        else if (onMenu3 == 1) {
-            system("cls");
-            cout << "<Goi ham la trang khi ma chon vao 1 OPTION co onMenu3=1 (OPTION 1)>" << endl;
+		}
+		else if (onMenu3 == 1) {
+			system("cls");
+			cout << "<Goi ham la trang khi ma chon vao 1 OPTION co onMenu3=1 (OPTION 1)>" << endl;
 			system("pause");
-            //.<Goi ham la trang khi ma chon vao 1 OPTION co onMenu3=1 (OPTION 1)>
-            onMenu3 = 0;//.Sau ham se la: onMenu3 = 0 de khi ta nhan 'q' de thoat khoi ham(trang) do thi no se dat onMenu3=0 de in ra lai trang MẸ
-        }
-        else if (onMenu3 == 100) {
-            break;
-        }
-    } while (key != 'q');
+			//.<Goi ham la trang khi ma chon vao 1 OPTION co onMenu3=1 (OPTION 1)>
+			onMenu3 = 0;//.Sau ham se la: onMenu3 = 0 de khi ta nhan 'q' de thoat khoi ham(trang) do thi no se dat onMenu3=0 de in ra lai trang MẸ
+		}
+		else if (onMenu3 == 100) {
+			break;
+		}
+	} while (key != 'q');
 
 }
